626a4b66b9c36e4aafdacf0d4e1fff3d28167e48
root <root@euca-10-1-4-135.eucalyptus.internal>
2017-07-16 11:16:32 -0700

    basic runs test as sequential C

--- apps/runs-test/c-runstest.c
+++ apps/runs-test/c-runstest.c
@@ -2,8 +2,82 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 #include "c-twist.h"
+
+// from https://en.wikipedia.org/wiki/Wald–Wolfowitz_runs_test
+double RunsStat(double *v, int N)
+{
+	int i;
+	double n_plus = 0;
+	double n_minus = 0;
+	double mu;
+	double sigma_sq;
+	double stat;
+	double runs;
+	int b1;
+	int b2;
+
+	if(N == 0) {
+		return(-1);
+	}
+
+	for(i=0; i < N; i++) {
+		if(v[i] >= 0.5) {
+			n_plus++;
+		} else {
+			n_minus++;
+		}
+	}
+
+	mu = ((2*n_plus*n_minus) / (double)N) + 1.0;
+	sigma_sq = ((mu - 1) * (mu - 2)) / ((double)N-1.0);
+
+	runs = 1;
+
+	for(i=1; i < N; i++) {
+		if(v[i-1] >= 0.5) {
+			b1 = 1;
+		} else {
+			b1 = 0;
+		}
+		if(v[i] >= 0.5) {
+			b2 = 1;
+		} else {
+			b2 = 0;
+		}
+
+#ifdef DEBUG
+		if(b1 == 1) {
+			printf("+");
+		} else {
+			printf("-");
+		}
+#endif
+
+		if(b1 != b2) {
+			runs++;
+		}
+	}
+
+
+#ifdef DEBUG
+	if(b2 == 1) {
+		printf("+\n");
+	} else {
+		printf("-\n");
+	}
+	printf("runs: %f\n",runs);
+	fflush(stdout);
+#endif
+
+	stat = (runs - mu) / sqrt(sigma_sq);
+
+	return(stat);
+}
+
+#ifdef STANDALONE 
 
 #define ARGS "c:TS:"
 char *Usage = "usage: c-runstest -c count\n\
@@ -20,7 +94,8 @@ int main(int argc, char **argv)
 	int i;
 	int has_seed;
 	struct timeval tm;
-	double r;
+	double *r;
+	double stat;
 
 	count = 10;
 	UseThread = 0;
@@ -46,22 +121,35 @@ int main(int argc, char **argv)
 		}
 	}
 
+	if(count <= 0) {
+		fprintf(stderr,"count must be non-negative\n");
+		fprintf(stderr,"%s",Usage);
+		exit(1);
+	}
+
 	if(has_seed == 0) {
 		gettimeofday(&tm,NULL);
 		Seed = (uint32_t)((tm.tv_sec + tm.tv_usec) % 0xFFFFFFFF);
 	}
 
+	r = (double *)malloc(count * sizeof(double));
+	if(r == NULL) {
+		fprintf(stderr,"no space\n");
+		exit(1);
+	}
+
 	CTwistInitialize(Seed);
 
 	for(i=0; i < count; i++) {
-		r = CTwistRandom();
-		printf("r: %f\n",r);
-		fflush(stdout);
+		r[i] = CTwistRandom();
+//		printf("r: %f\n",r[i]);
+//		fflush(stdout);
 	}
+
+	stat = RunsStat(r,count);
+	printf("runs statistic: %f\n",stat);
 
 	return(0);
 }
 
-	
-				
-
+#endif
