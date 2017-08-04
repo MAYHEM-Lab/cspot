a4b469c4ee47e4830137065f6880a4c27873dd1d
root <root@euca-10-1-5-30.eucalyptus.internal>
2017-08-04 10:37:01 -0700

    I can't believe it's a girdle

--- zmq/zproxy.c
+++ zmq/zproxy.c
@@ -9,8 +9,8 @@ int main (void)
 	void *frontend = zmq_socket (context, ZMQ_PULL);
 	void *backend = zmq_socket (context, ZMQ_PUSH);
 // Bind both sockets to TCP ports
-	zmq_bind (frontend, "tcp://*:6028");
-	zmq_bind (backend, "tcp://*:6029");
+	zmq_bind (frontend, "tcp://*:6030");
+	zmq_bind (backend, "tcp://10.1.5.30:6029");
 // Start the queue proxy, which runs until ETERM 
 	zmq_proxy (frontend, backend, NULL);
 
--- zmq/zserver.c
+++ zmq/zserver.c
@@ -1,18 +1,37 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <zmq.h>
 #include <czmq.h>
 
 int main (void)
 {
-    zsock_t *pull = zsock_new_pull (">tcp://127.0.0.1:6029");
+//	zctx_t *context = zctx_new();
+//	void *socket = zsocket_new(context,ZMQ_PULL);
 
-    char *string = zstr_recv(pull);
-    printf("%s",string);
-    fflush(stdout);
+	zsock_t *pull = zsock_new_pull ("@tcp://*:6029");
+
+	if(pull == NULL) {
+		fprintf(stderr,"zsock new failed\n");
+		fflush(stderr);
+		exit(1);
+	}
+
+//	zsocket_bind(socket,"tcp://*:6029");
+//	zsock_bind(pull,"tcp://10.1.5.30:6029");
+
+
+	char *string = zstr_recv(pull);
+//	char *string = zstr_recv(socket);
+	printf("%s",string);
+	fflush(stdout);
+
 //    zstr_send(pull,"done");
 
-    zstr_free (&string);
-    zsock_destroy (&pull);
+	zstr_free (&string);
+	zsock_destroy (&pull);
+//	zsocket_destroy(context,socket);
+//	zctx_destroy(&context);
+
     return 0;
 }
 
