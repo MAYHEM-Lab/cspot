80b0d5e0d98c069395127c6c53cbf44097b26141
root <root@euca-10-1-5-30.eucalyptus.internal>
2017-08-24 16:27:57 -0700

    a few fixes

--- apps/put-test/recv_start.c
+++ apps/put-test/recv_start.c
@@ -12,35 +12,29 @@ int recv_start(WOOF *wf, unsigned long seq_no, void *ptr)
 {
 
 	PT_EL el;
-	WOOF *target;
-	WOOF *woof_log;
 	char target_name[4096];
 	char log_name[4096];
+	int err;
 
 	memcpy(&el,ptr,sizeof(el));
 
-	target = WooFCreate(el.target_name, el.element_size, el.history_size);
-	if(target == NULL) {
+	err = WooFCreate(el.target_name, el.element_size, el.history_size);
+	if(err < 0) {
 		fprintf(stderr,"recv_init: failed to create target woof %s\n",
 			target_name);
 		fflush(stderr);
 		return(-1);
 	}
 
-	WooFFree(target);
-
 	/*
 	 * create a log WOOF to contain timestamps for received elements
 	 */
-	woof_log = WooFCreate(el.log_name,sizeof(EX_LOG),el.history_size);
-	if(woof_log == NULL) {
+	err = WooFCreate(el.log_name,sizeof(EX_LOG),el.history_size);
+	if(err < 0) {
 		fprintf(stderr,"recv_init: failed to create woof log\n");
 		fflush(stderr);
-		WooFFree(target);
 		return(-1);
 	}
-
-	WooFFree(woof_log);
 
 	return(1);
 }
