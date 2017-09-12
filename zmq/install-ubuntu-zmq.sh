d7f12101aa9d32dcb6bb7024ae2590b28db83135
Mr. Greenjeans <rich.wolski@gmail.com>
2017-09-12 15:51:06 -0700

    ubuntu version

---
+++ zmq/install-ubuntu-zmq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+wget https://github.com/zeromq/libzmq/releases/download/v4.2.1/zeromq-4.2.1.tar.gz
+cd zeromq-4.2.1/
+tar -xvzf zeromq-4.2.1.tar.gz
+
+sudo apt-get install libtool pkg-config build-essential autoconf automake uuid-dev
+sudo apt-get install checkinstall
+
+./configure
+make
+sudo checkinstall
+sudo ldconfig
+
+cd ..
+
+mkdir -p /src
+cd /src
+git clone git://github.com/zeromq/czmq.git
+cd czmq
+./autogen.sh && ./configure && make check
+make install
+ldconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
