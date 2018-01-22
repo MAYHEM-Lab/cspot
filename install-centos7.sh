bb518de204657018f2bea90061dcd2e4f758bb96
Destructo <rich@cs.ucsb.edu>
2018-01-22 12:07:47 -0800

    need the matrix stuff

--- install-centos7.sh
+++ install-centos7.sh
@@ -5,13 +5,14 @@ HERE=`pwd`
 cd ..
 git clone https://github.com/richwolski/euca-cutils.git
 git clone https://richwolski@github.com/MAYHEM-Lab/mio.git
+git clone https://richwolski@github.com/MAYHEM-Lab/matrix.git
 
 cd euca-cutils; make
 cd ..
 cd mio; make
 cd $HERE
 
-yum -y install docker
+yum -y install docker gcc-gfortran
 systemctl start docker
 systemctl enable docker
 
