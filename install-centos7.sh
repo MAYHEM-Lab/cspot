4d283e46a1e0f395184778dc08a96f27ff940d93
root <root@euca-10-1-5-13.eucalyptus.internal>
2018-01-31 12:11:10 -0800

    added deps for apps/regress-pair

--- install-centos7.sh
+++ install-centos7.sh
@@ -2,17 +2,24 @@
 
 
 HERE=`pwd`
+yum -y install docker gcc-gfortran
 cd ..
 git clone https://github.com/richwolski/euca-cutils.git
 git clone https://richwolski@github.com/MAYHEM-Lab/mio.git
 git clone https://richwolski@github.com/MAYHEM-Lab/matrix.git
+git clone https://richwolski@github.com/MAYHEM-Lab/distributions.git
+git clone https://richwolski@github.com/MAYHEM-Lab/ssa-changepoint.git
 
 cd euca-cutils; make
 cd ..
 cd mio; make
+cd distributions; make
+cd ..
+cd matrix; ./install-matrix.sh
+cd ..
+cd ssa-changepoint; make
 cd $HERE
 
-yum -y install docker gcc-gfortran
 systemctl start docker
 systemctl enable docker
 
