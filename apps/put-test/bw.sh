81e3b7b2568dd60a2aaf85305bbdbe9c9414deb1
Rich Wolski <rich@cs.ucsb.edu>
2018-04-20 08:48:52 -0700

    need this too

--- apps/put-test/bw.sh
+++ apps/put-test/bw.sh
@@ -24,13 +24,19 @@ BATCHCOUNT=$(($COUNT / 4))
 CNT=0
 
 TOTAL=0
+MIN=9999999999
 while ( test $CNT -lt $BATCHCOUNT ) ; do
 	SUM4=`./stress-test -W woof://$HERE/zzzstress -s 4 -g 1 -p 1 -P $PAYLOAD | grep elapsed | awk '{print $4}'`
+	MIN=`echo $SUM4 -v min=$MIN '{if($1 < min){print $1}else{print min}}'`
+	MIN=`echo $SUM4 -v min=$MIN '{if($2 < min){print $1}else{print min}}'`
+	MIN=`echo $SUM4 -v min=$MIN '{if($3 < min){print $1}else{print min}}'`
+	MIN=`echo $SUM4 -v min=$MIN '{if($4 < min){print $1}else{print min}}'`
 	SUM=`echo $SUM4 | awk '{print $1+$2+$3+$4}'`
 #echo SUM4: $SUM4 SUM: $SUM
 	TOTAL=`echo $TOTAL $SUM  | awk '{print $1+$2}'`
 	CNT=$(($CNT+1))
 done
 BW=`echo $TOTAL $PAYLOAD $COUNT | awk '{print ($2*$3)/($1/1000.0)}'`
-echo payload: $PAYLOAD count: $COUNT time: $TOTAL bw: $BW bytes/s
+BEST=`echo $PAYLOAD $MIN | awk '{print ($1/(1024*1024)) / ($2 / 1000.0)}'`
+echo payload: $PAYLOAD count: $COUNT time: $TOTAL bw: $BW bytes/s min: $MIN best: $BEST bytes/s
 
