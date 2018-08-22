3965226f5a6d60396205ac5ba5c4bcda69a8c03e
Gareth George <garethgeorge97@gmail.com>
2018-08-22 23:47:39 +0000

    woofc-namespace-platform -m and -M properties for launching multiple containers no longer causes a port collision

--- woofc-host.c
+++ woofc-host.c
@@ -314,27 +314,64 @@ void *WooFContainerLauncher(void *arg)
 
 		port = WooFPortHash(WooF_namespace);
 
-		sprintf(launch_string, "docker run -t\
-			 -e LD_LIBRARY_PATH=/usr/local/lib\
-			 -e WOOFC_NAMESPACE=%s\
-			 -e WOOFC_DIR=%s\
-			 -e WOOF_NAME_ID=%lu\
-			 -e WOOF_NAMELOG_NAME=%s\
-			 -e WOOF_HOST_IP=%s\
-			 -p %d:%d\
-			 -v %s:%s\
-			 -v %s:/cspot-namelog\
-			 cspot-docker-centos7\
-			 %s/%s",
-				WooF_namespace,
-				pathp,
-				Name_id,
-				Namelog_name,
-				Host_ip,
-				port,port,
-				WooF_dir,pathp,
-				WooF_namelog_dir, /* all containers find namelog in /cspot-namelog */
-				pathp,"woofc-container");
+		// begin constructing the launch string 
+		sprintf(launch_string + strlen(launch_string),
+			"docker run -t "
+			"-e LD_LIBRARY_PATH=/usr/local/lib "
+			"-e WOOFC_NAMESPACE=%s "
+			"-e WOOFC_DIR=%s "
+			"-e WOOF_NAME_ID=%lu "
+			"-e WOOF_NAMELOG_NAME=%s "
+			"-e WOOF_HOST_IP=%s ", 
+			 WooF_namespace,
+			 pathp,
+			 Name_id,
+			 Namelog_name,
+			 Host_ip);
+
+		if (count == 0) {
+			sprintf(launch_string + strlen(launch_string), 
+				"-p %d:%d ", port, port);
+		}
+
+		sprintf(launch_string + strlen(launch_string), 
+			"-v %s:%s "
+			"-v %s:/cspot-namelog "
+			"cspot-docker-centos7 "
+			"%s/%s ",
+			WooF_dir,pathp,
+			WooF_namelog_dir, /* all containers find namelog in /cspot-namelog */
+			pathp,"woofc-container"
+			);
+
+		if (count == 0) {
+			sprintf(launch_string + strlen(launch_string), "-M ");
+		}
+
+		fprintf(stdout, "\tcommand: '%s'\n", launch_string);
+		fflush(stdout);
+
+		// sprintf(launch_string, "docker run -t\
+		// 	 -e LD_LIBRARY_PATH=/usr/local/lib\
+		// 	 -e WOOFC_NAMESPACE=%s\
+		// 	 -e WOOFC_DIR=%s\
+		// 	 -e WOOF_NAME_ID=%lu\
+		// 	 -e WOOF_NAMELOG_NAME=%s\
+		// 	 -e WOOF_HOST_IP=%s\
+		// 	 -p %d:%d\
+		// 	 -v %s:%s\
+		// 	 -v %s:/cspot-namelog\
+		// 	 cspot-docker-centos7\
+		// 	 %s/%s %s",
+		// 		WooF_namespace,
+		// 		pathp,
+		// 		Name_id,
+		// 		Namelog_name,
+		// 		Host_ip,
+		// 		port,port,
+		// 		WooF_dir,pathp,
+		// 		WooF_namelog_dir, /* all containers find namelog in /cspot-namelog */
+		// 		pathp,"woofc-container",flags);
 
 		err = pthread_create(&tid,NULL,WooFDockerThread,(void *)launch_string);
 		if(err < 0) {
