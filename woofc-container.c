8f089ebec73839f33ca20e28457bd75cc6e09a5a
Fatih BAKIR <fatih@linux.com>
2018-09-17 21:14:11 -0700

    namespace to woofc_namespace

--- woofc-container.c
+++ woofc-container.c
@@ -382,7 +382,7 @@ void *WooFForker(void *arg)
 			   (ev[first].seq_no > last_seq_no)) {
 				/* now walk forward looking for FIRING */
 #ifdef DEBUG
-	printf("WooFForker: considering %s %llu\n",ev[first].namespace,ev[first].seq_no);
+	printf("WooFForker: considering %s %llu\n",ev[first].woofc_namespace,ev[first].seq_no);
 	fflush(stdout);
 #endif
 				firing = (first - 1);
