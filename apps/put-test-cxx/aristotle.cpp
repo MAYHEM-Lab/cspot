c23afebfa717526051bf0dbf94004e72c2a7566c
Fatih BAKIR <fatih@linux.com>
2018-09-19 13:10:03 -0700

    updated ip addresses for experiment

--- apps/put-test-cxx/aristotle.cpp
+++ apps/put-test-cxx/aristotle.cpp
@@ -1,4 +1,4 @@
 #include <string>
 
-std::string next_hop = "woof://127.0.0.1/put-test-cxx/data_woof";
+std::string next_hop = "woof://34.220.203.129/put-test-cxx/data_woof";
 int my_index = 3;
--- apps/put-test-cxx/aws.cpp
+++ apps/put-test-cxx/aws.cpp
@@ -1,4 +1,4 @@
 #include <string>
 
-std::string next_hop;// = "woof://127.0.0.1/put-test-cxx/data_woof";
+std::string next_hop;
 int my_index = 4;
