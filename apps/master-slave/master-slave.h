f424164aa493d57932b9824880d96b91dc405c38
Rich <rich@caprine.local>
2018-11-01 17:24:26 -0700

    working on a compile

--- apps/master-slave/master-slave.h
+++ apps/master-slave/master-slave.h
@@ -1,13 +1,15 @@
 #ifndef MASTER_SLAVE_H
 #define MASTER_SLAVE_H
 
+#define IPSTRLEN (15)
+
 struct state_stc
 {
 	char wname[4096];
 	char my_ip[IPSTRLEN+1];
 	unsigned char my_state; /* M, S, C, 0 */
 	char other_ip[IPSTRLEN+1];
-	unsigned char other_state /* M, S, C, 0 */ 
+	unsigned char other_state; /* M, S, C, 0 */ 
 	char other_color; /* R=>disconnected, G=>connected */
 	char client_ip[IPSTRLEN+1];
 	char client_color; /* R, G */
@@ -27,26 +29,16 @@ struct pingpong_stc
 {
 	unsigned long seq_no;
 	char return_woof[1024];
-}
+};
 typedef struct pingpong_stc PINGPONG;
 
 #define PPRETRIES (6)
 #define PPSLEEP (5)
-
-struct pulse_stc
-{
-	wname[4096];
-	struct timeval tm;
-	unsigned long last_seq_no;
-};
-
-typedef struct pulse_stc PULSE;
 
 #define MAKE_EXTENDED_NAME(ename,wname,str) {\
         memset(ename,0,sizeof(ename));\
         sprintf(ename,"%s.%s",wname,str);\
 }
 
-#define IPSTRLEN (15)
 #endif
 
