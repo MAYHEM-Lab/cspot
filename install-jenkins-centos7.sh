b3c27ee8dd1ef82071bf84ce91e28bf99d85aa0a
Rich <rich@169-231-166-184.wireless.ucsb.edu>
2018-12-10 12:54:54 -0800

    trying to set up jenkins

---
+++ install-jenkins-centos7.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+HERE=`pwd`
+yum -y install docker gcc-gfortran python-devel python34-devel
+cd ..
+git clone https://github.com/richwolski/euca-cutils.git
+git clone git@github.com:MAYHEM-Lab/mio.git
+git clone git@github.com:MAYHEM-Lab/matrix.git
+git clone git@github.com:MAYHEM-Lab/distributions.git
+git clone git@github.com:MAYHEM-Lab/ssa-changepoint.git
+
+cd euca-cutils; make
+cd ..
+cd mio; make
+cd ..
+cd distributions; make
+cd ..
+cd matrix; ./install-matrix.sh
+cd ..
+cd ssa-changepoint; make
+cd $HERE
+
+systemctl start docker
+systemctl enable docker
+
+cd zmq
+./install-zmq.sh
+cd ..
+make
