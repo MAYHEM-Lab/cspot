2dc9b987c472ada8c3c6f198d7cf17ead48ed417
Wei-Tsung Lin <fasthall@gmail.com>
2019-01-30 16:39:57 -0800

    FINALLY WORKING find all woofs need to be repaired

--- repair.h
+++ repair.h
@@ -14,11 +14,13 @@
 
 void LogInvalidByWooF(LOG *log, unsigned long woofc_seq_no);
 void LogInvalidByCause(LOG *log, unsigned ndx, unsigned long cause_host, unsigned long cause_seq_no);
-void GLogMarkWooFDownstream(GLOG *glog, unsigned long host, char *woof_name, unsigned long woofc_seq_no);
+int GLogMarkWooFDownstream(GLOG *glog, unsigned long host, char *woof_name, unsigned long woofc_seq_no);
 void GLogMarkWooFDownstreamRange(GLOG *glog, unsigned long host, char *woof_name, unsigned long woofc_seq_no_begin, unsigned long woofc_seq_no_end);
-void GLogMarkEventDownstream(GLOG *glog, unsigned ndx, unsigned long cause_host, unsigned long cause_seq_no);
-void GLogFindReplacedWooF(GLOG *glog, unsigned long host, char *woof_name, Dlist *holes);
-void GLogFindAffectedWooF(GLOG *glog, RB *woof_put, int *count_woof_put, RB *woof_get, int *count_woof_get);
+void GLogMarkEventDownstream(GLOG *glog, unsigned ndx, unsigned long cause_host, unsigned long cause_seq_no,
+                             char *woof_name, unsigned long woof_seq_no);
+void GLogFindRootEvent(GLOG *glog, unsigned long ndx, unsigned long *host, unsigned long *seq_no, unsigned long *root_ndx);
+void GLogFindWooFHoles(GLOG *glog, unsigned long host, char *woof_name, Dlist *holes);
+void GLogFindAffectedWooF(GLOG *glog, RB *root, int *count_root, RB *casualty, int *count_casualty, RB *progress, int *count_progress);
 char *KeyFromWooF(unsigned long host, char *woof);
 int WooFFromHval(char *key, unsigned long *host, char *woof);
 #endif
