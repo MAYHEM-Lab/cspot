b39dd39d420e4f2af2427fdbbc5aea171d2cf1e0
Wei-Tsung Lin <fasthall@gmail.com>
2019-10-30 10:47:21 -0700

    add some deps to makefiles

--- apps/replica/slave.c
+++ apps/replica/slave.c
@@ -16,11 +16,10 @@
 #include "woofc-host.h"
 #include "woofc-access.h"
 
-#define ARGS "m:p:f:"
-char *Usage = "slave -m master_namespace -p port -f log_name\n";
+#define ARGS "m:p:"
+char *Usage = "slave -m master_namespace -p port\n";
 
 extern LOG *Name_log;
-char log_name[4096];
 
 int main(int argc, char **argv)
 {
@@ -30,23 +29,24 @@ int main(int argc, char **argv)
 	unsigned long history_size;
 	unsigned long element_size;
 	unsigned long seq_no;
-	LOG *log;
-	MIO *lmio;
 	EVENT *ev_array;
 	EVENT ev;
 	char woof_name[4096];
 	unsigned long i;
 	struct stat sbuf;
-	WOOF *woof;
-	void *buffer;
+	char woof_element[4096];
 	unsigned long master_seq, slave_seq;
 	unsigned long num_events;
-
 	int port = 8080;
 	int sockfd = 0, n = 0;
 	char buf[2048];
 	char master_ip[1024];
 	struct sockaddr_in serv_addr;
+
+	unsigned long last_seqno = 0;
+	struct timeval begin, end;
+	struct timeval t1, t2;
+	double elapsedTime = 0;
 
 	while ((c = getopt(argc, argv, ARGS)) != EOF)
 	{
@@ -54,9 +54,6 @@ int main(int argc, char **argv)
 		{
 		case 'm':
 			strncpy(master_namespace, optarg, sizeof(master_namespace));
-			break;
-		case 'f':
-			strncpy(log_name, optarg, sizeof(log_name));
 			break;
 		case 'p':
 			port = atoi(optarg);
@@ -68,9 +65,9 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (log_name[0] == 0 || master_namespace[0] == 0)
+	if (master_namespace[0] == 0)
 	{
-		fprintf(stderr, "must specify log name\n");
+		fprintf(stderr, "must specify master_namespace\n");
 		fprintf(stderr, "%s", Usage);
 		exit(1);
 	}
@@ -84,47 +81,37 @@ int main(int argc, char **argv)
 
 	WooFInit();
 
-	lmio = MIOReOpen(log_name);
-	if (lmio == NULL)
-	{
-		fprintf(stderr, "couldn't open mio for log %s\n", log_name);
-		fflush(stderr);
-		exit(1);
-	}
-	log = (LOG *)MIOAddr(lmio);
-	if (log == NULL)
-	{
-		fprintf(stderr, "couldn't open log as %s\n", log_name);
-		fflush(stderr);
-		exit(1);
-	}
-
 	ev_array = (EVENT *)(((void *)Name_log) + sizeof(LOG));
-	printf("log head: %lu\n", Name_log->head);
-	printf("log tail: %lu\n", Name_log->tail);
-	printf("log head seqno: %lu\n", ev_array[Name_log->head].seq_no);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
 	inet_pton(AF_INET, master_ip, &serv_addr.sin_addr);
-
+	
 	while (1)
 	{
 		connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
 		// send the latest event seqno to master
 		slave_seq = ev_array[Name_log->head].seq_no;
+		if (slave_seq == 0)
+		{
+			gettimeofday(&begin, NULL);
+		}
 		sprintf(buf, "%lu", slave_seq);
 		// printf("slave_seq: %lu\n", slave_seq);
-		write(sockfd, buf, sizeof(buf));
 
+		gettimeofday(&t1, NULL);
+		write(sockfd, buf, sizeof(buf));
 		n = read(sockfd, buf, sizeof(buf));
 		while (n < sizeof(buf))
 		{
 			n += read(sockfd, buf + n, sizeof(buf) - n);
 		}
+		gettimeofday(&t2, NULL);
+		elapsedTime += (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
+
 		num_events = strtoul(buf, (char **)NULL, 10);
 		if (num_events > 0)
 		{
@@ -132,11 +119,14 @@ int main(int argc, char **argv)
 		}
 		for (i = 0; i < num_events; i++)
 		{
+			gettimeofday(&t1, NULL);
 			n = read(sockfd, buf, sizeof(buf));
 			while (n < sizeof(buf))
 			{
 				n += read(sockfd, buf + n, sizeof(buf) - n);
 			}
+			gettimeofday(&t2, NULL);
+			elapsedTime += (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
 			memcpy(&ev, buf, sizeof(EVENT));
 			// printf("host: %lu seq_no: %llu r_host: %lu r_seq_no: %llu woofc_seq_no: %lu woof: %s type: %d timestamp: %lu\n",
 			// 	ev.host,
@@ -154,9 +144,24 @@ int main(int argc, char **argv)
 				sprintf(woof_name, "%s/%s", master_namespace, ev.woofc_name);
 				element_size = WooFMsgGetElSize(woof_name);
 				// history_size = WooFMsgGetHistorySize(ev.woofc_name);
-				history_size = 10000;
+				history_size = 20000;
+				if (element_size == 16) // BST_LL_AP
+				{
+					history_size = 10000;
+				}
+				else if (element_size == 32) // BST_LL_LINK
+				{
+					history_size = 20000;
+				}
+				else if (element_size == 520) // BST_DATA
+				{
+					history_size = 20000;
+				}
+				else if (element_size == 528) // LL_DATA
+				{
+					history_size = 10000;
+				}
 
-				// woof = WooFOpen(woof_name);
 				err = WooFCreate(ev.woofc_name, element_size, history_size);
 				if (err < 0)
 				{
@@ -164,33 +169,36 @@ int main(int argc, char **argv)
 					exit(1);
 				}
 			}
-			if (ev.type == LATEST_SEQNO)
-			{
-				WooFGetLatestSeqno(ev.woofc_name);
-				// printf("WooFGetLatestSeqno(ev.woofc_name);\n");
-			}
-			else if (ev.type == READ)
-			{
-				woof = WooFOpen(ev.woofc_name);
-				buffer = (void *)malloc(woof->shared->element_size);
-				WooFDrop(woof);
-				WooFGet(ev.woofc_name, buffer, ev.woofc_seq_no);
-				free(buffer);
-				// printf("WooFGet(ev.woofc_name, buffer, ev.woofc_seq_no);\n");
-			}
-			else if (ev.type == APPEND)
-			{
-				woof = WooFOpen(ev.woofc_name);
-				buffer = (void *)malloc(woof->shared->element_size);
-				WooFDrop(woof);
-				sprintf(woof_name, "%s/%s", master_namespace, ev.woofc_name);
-				// WooFGetWithoutEvent(woof_name, buffer, ev.woofc_seq_no);
-				WooFGet(woof_name, buffer, ev.woofc_seq_no);
-				seq_no = WooFPut(ev.woofc_name, NULL, buffer);
-				free(buffer);
-			}
+			// if (ev.type == LATEST_SEQNO)
+			// {
+			// 	WooFGetLatestSeqno(ev.woofc_name);
+			// 	// printf("WooFGetLatestSeqno(ev.woofc_name);\n");
+			// }
+			// else if (ev.type == READ)
+			// {
+			// 	WooFGet(ev.woofc_name, &woof_element, ev.woofc_seq_no);
+			// }
+			// else if (ev.type == APPEND)
+			// {
+			// 	sprintf(woof_name, "%s/%s", master_namespace, ev.woofc_name);
+			// 	WooFGet(woof_name, &woof_element, ev.woofc_seq_no);
+			// 	seq_no = WooFPut(ev.woofc_name, NULL, &woof_element);
+			// }
+			ev_array[Name_log->head].seq_no++;
 		}
-		fflush(stdout);
+		if (ev_array[Name_log->head].seq_no == last_seqno)
+		{
+			gettimeofday(&end, NULL);
+			printf("total: %f ms\n", (end.tv_sec - begin.tv_sec) * 1000.0 + (end.tv_usec - begin.tv_usec) / 1000.0);
+			printf("communication: %f ms\n", elapsedTime);
+			fflush(stdout);
+			memset(buf, 0, sizeof(buf));
+			sprintf(buf, "-1");
+			write(sockfd, buf, sizeof(buf));
+			break;
+		}
+		printf("seqno: %lu\n", ev_array[Name_log->head].seq_no);
+		last_seqno = ev_array[Name_log->head].seq_no;
 		usleep(100000);
 	}
 
