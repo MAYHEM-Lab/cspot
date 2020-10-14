38e148c61f463de5a786adf0332f4bed47a53a86
Fatih Bakir <mfatihbakir@gmail.com>
2020-10-14 15:04:33 -0700

    A fatal_if for assert-ish functionality

--- include/debug.h
+++ include/debug.h
@@ -7,10 +7,12 @@ extern "C" {
 
 void cspot_print_debug(const char* format, ...);
 void cspot_print_fatal(const char* format, ...) __attribute__((noreturn));
+void cspot_print_fatal_if(bool val, const char* format, ...);
 
 #define DEBUG_LOG(...) cspot_print_debug(__VA_ARGS__)
 #define DEBUG_WARN(...) cspot_print_debug(__VA_ARGS__)
 #define DEBUG_FATAL(...) cspot_print_fatal(__VA_ARGS__)
+#define DEBUG_FATAL_IF(cond, ...) cspot_print_fatal_if(cond, __VA_ARGS__)
 
 #if defined(__cplusplus)
 }
--- src/debug.cpp
+++ src/debug.cpp
@@ -30,3 +30,18 @@ void cspot_print_fatal(const char* format, ...) {
     va_end(myargs);
     std::terminate();
 }
+
+void cspot_print_fatal_if(bool val, const char* format, ...) {
+    if (val) {
+        std::cerr << "[fatal] ";
+        va_list myargs;
+        va_start(myargs, format);
+        [[maybe_unused]] auto ret = vfprintf(stderr, format, myargs);
+        if (format[strlen(format) - 1] != '\n') {
+            std::cerr << '\n';
+        }
+        fflush(stderr);
+        va_end(myargs);
+        std::terminate();
+    }
+}
