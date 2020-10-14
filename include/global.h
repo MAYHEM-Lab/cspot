7902dff401f8b5e39d8c25b5fb1c96d7846d4bca
Fatih Bakir <mfatihbakir@gmail.com>
2020-10-14 16:00:18 -0700

    C++ convenience functions to set globals

--- include/global.h
+++ include/global.h
@@ -11,10 +11,21 @@ extern char WooF_dir[2048];
 extern char WooF_namelog_dir[2048];
 extern char Namelog_name[2048];
 extern unsigned long Name_id;
-extern LOG* Name_log;
 extern char Host_ip[25];
 
+extern LOG* Name_log;
+
 #if defined(__cplusplus)
+}
+
+#include <string_view>
+
+namespace cspot::globals {
+void set_namespace(std::string_view ns);
+void set_dir(std::string_view dir);
+void set_namelog_dir(std::string_view dir);
+void set_namelog_name(std::string_view name);
+void set_host_ip(std::string_view ip);
 }
 #endif
 #endif
--- src/global.cpp
+++ src/global.cpp
@@ -1,4 +1,5 @@
 #include <global.h>
+#include <cstring>
 
 extern "C" {
 char WooF_namespace[2048];
@@ -8,4 +9,22 @@ char Namelog_name[2048];
 unsigned long Name_id;
 LOG* Name_log;
 char Host_ip[25];
+}
+
+namespace cspot::globals {
+void set_namespace(std::string_view ns) {
+    strncpy(WooF_namespace, ns.data(), std::min(std::size(WooF_namespace), ns.size()));
+}
+void set_dir(std::string_view dir) {
+    strncpy(WooF_dir, dir.data(), std::min(std::size(WooF_dir), dir.size()));
+}
+void set_namelog_dir(std::string_view dir) {
+    strncpy(WooF_namelog_dir, dir.data(), std::min(std::size(WooF_namelog_dir), dir.size()));
+}
+void set_namelog_name(std::string_view name) {
+    strncpy(Namelog_name, name.data(), std::min(std::size(Namelog_name), name.size()));
+}
+void set_host_ip(std::string_view ip) {
+    strncpy(Host_ip, ip.data(), std::min(std::size(Host_ip), ip.size()));
+}
 }
