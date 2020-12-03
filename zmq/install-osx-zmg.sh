8e2c67fc4b05973c0a8c57814f4a316007907609
Fatih Bakir <mfatihbakir@gmail.com>
2020-12-02 17:04:46 -0800

    Remove zmq dir

--- zmq/install-osx-zmg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
-brew install czmq
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
