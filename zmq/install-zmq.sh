ff8cf650f844be6a7d28d6e10f54b93864e3aa28
root <root@euca-10-1-4-47.eucalyptus.internal>
2021-11-09 19:54:10 +0000

    need this to fix the build

--- zmq/install-zmq.sh
+++ zmq/install-zmq.sh
@@ -8,6 +8,9 @@ mkdir -p /src
 cd /src
 git clone git://github.com/zeromq/czmq.git
 cd czmq
+echo "#define ZMQ_ROUTING_ID (0)" > /tmp/temp.h
+cat /tmp/temp.h include/czmq.h > /tmp/temp1.h
+mv /tmp/temp1.h include/czmq.h
 ./autogen.sh && ./configure && make check
 make install
 ldconfig
