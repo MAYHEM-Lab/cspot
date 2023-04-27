874471346605ea9e69daaf2f0b9fe53f0b5bf79a
root <root@ip-172-31-45-231.compute.internal>
2023-04-27 19:18:25 +0000

    will try and fix github actions next

--- install-centos8.sh
+++ install-centos8.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 
-#yum -y install centos-release-scl && yum -y install devtoolset-9
-#yum -y install centos-release-scl && sudo yum -y install devtoolset-9
 dnf -y install gcc-toolset-9-gcc gcc-toolset-9-gcc-c++
 GTEST=`grep gcc-toolset-9 ~/.bashrc | grep enable`
 if ( test -z "$GTEST" ) ; then
 	echo "source /opt/rh/gcc-toolset-9/enable" >> ~/.bashrc
 fi	
-yum -y install glibc-static unzip
-yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+dnf -y install glibc-static unzip
+dnf -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 yum install -y yum-utils
-yum -y install wget
+dnf -y install wget
 yum -y erase podman buildah
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum -y install docker-ce docker-ce-cli containerd.io
@@ -24,21 +22,14 @@ wget https://github.com/Kitware/CMake/releases/download/v3.19.1/cmake-3.19.1-Lin
 chmod +x cmake-3.19.1-Linux-x86_64.sh 
 ./cmake-3.19.1-Linux-x86_64.sh --skip-license --prefix=/usr
 yum -y localinstall https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/c/czmq-3.0.2-3.el7.x86_64.rpm
-#git clone --branch release-2.0 https://github.com/Mayhem-lab/cspot
 cd cspot/
 git submodule update --init --recursive
 mkdir build
 cd build/
-#echo "#!/bin/bash" > helper.sh
-#echo "cmake -G Ninja .." >> helper.sh
-#echo "ninja" >> helper.sh
-#echo "ninja install" >> helper.sh
-#chmod 755 helper.sh
 source ~/.bashrc
 cmake -G Ninja ..
 ninja
 ninja install
-cp ../SELF-TEST.sh ./bin
 #scl enable devtoolset-9 ./helper.sh
 docker pull racelab/cspot-docker-centos7
 docker tag racelab/cspot-docker-centos7 cspot-docker-centos7
@@ -47,5 +38,6 @@ if ! [[ $LD_LIBRARY_PATH == *"/usr/local/lib"* ]]; then
     echo -e "if ! [[ \$LD_LIBRARY_PATH == *\"/usr/local/lib\"* ]]; then\nexport  LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:/usr/local/lib\"\nfi" >> ~/.bashrc
     source ~/.bashrc
 fi
+cp ../SELF-TEST.sh ./bin
 cd ./bin
 ./SELF-TEST.sh
