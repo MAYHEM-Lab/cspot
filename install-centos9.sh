f3ef3fae97104834ccdd6fd30dc67e42cf924739
root <root@ip-172-31-47-230.compute.internal>
2023-05-08 15:26:56 -0700

    getting closer

--- install-centos9.sh
+++ install-centos9.sh
@@ -6,21 +6,21 @@
 
 
 dnf -y install gcc g++ wget scl-utils unzip
-dnf -y install glibc-static cmake
+dnf -y install glibc-static
 dnf -y install wget
 dnf -y remove podman runc
 curl https://download.docker.com/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
 sed -i -e "s/enabled=1/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
 dnf --enablerepo=docker-ce-stable -y install docker-ce
 systemctl start docker
-dnf --enablerepo=crb install ninja-build
+#dnf --enablerepo=crb install ninja-build
 cd ..
-#wget https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip
-#unzip ninja-linux.zip 
-#mv ninja /usr/bin/
-#wget https://github.com/Kitware/CMake/releases/download/v3.19.1/cmake-3.19.1-Linux-x86_64.sh
-#chmod +x cmake-3.19.1-Linux-x86_64.sh 
-#./cmake-3.19.1-Linux-x86_64.sh --skip-license --prefix=/usr
+wget https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip
+unzip ninja-linux.zip 
+mv ninja /usr/bin/
+wget https://github.com/Kitware/CMake/releases/download/v3.19.1/cmake-3.19.1-Linux-x86_64.sh
+chmod +x cmake-3.19.1-Linux-x86_64.sh 
+./cmake-3.19.1-Linux-x86_64.sh --skip-license --prefix=/usr
 yum -y localinstall https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/c/czmq-3.0.2-3.el7.x86_64.rpm
 cd cspot/
 git submodule update --init --recursive
@@ -28,6 +28,11 @@ echo "set(HAVE_SELECT 1)" > deps/libzmq/CMakeLists-notests.txt
 sed 's/build the tests" ON/build the tests" OFF/' deps/libzmq/CMakeLists.txt >> deps/libzmq/CMakeLists-notests.txt
 mv deps/libzmq/CMakeLists.txt deps/libzmq/CMakeLists-tests.txt
 mv deps/libzmq/CMakeLists-notests.txt deps/libzmq/CMakeLists.txt
+mv deps/libzmq/src/compat.hpp deps/libzmq/src/compat.orig.hpp
+echo "#define HAVE_STRNLEN" > deps/libzmq/src/compat.hpp
+cat deps/libzmq/src/compat.orig.hpp >> deps/libzmq/src/compat.hpp
+mv CMakeLists.txt CMakeLists.orig.txt
+mv CMakeLists-centos9.txt CMakeLists.txt
 mkdir build
 cd build/
 source ~/.bashrc
