ac9a02be512e09b16eb9db64be39a34ead3c940c
root <root@ip-172-31-47-246.compute.internal>
2023-05-11 15:20:44 -0700

    this might do it for csntos9

--- install-centos9.sh
+++ install-centos9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-dnf -y install gcc g++ unzip ninja-build cmake
+dnf -y install gcc g++ unzip
 dnf -y install glibc-static unzip
 dnf -y --enablerepo=crb install libstdc++-static
 dnf -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
@@ -12,12 +12,12 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum -y install docker-ce docker-ce-cli containerd.io
 systemctl start docker
 cd ..
-#wget https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip
-#unzip ninja-linux.zip 
-#mv ninja /usr/bin/
-#wget https://github.com/Kitware/CMake/releases/download/v3.19.1/cmake-3.19.1-Linux-x86_64.sh
-#chmod +x cmake-3.19.1-Linux-x86_64.sh 
-#./cmake-3.19.1-Linux-x86_64.sh --skip-license --prefix=/usr
+wget https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip
+unzip ninja-linux.zip 
+mv ninja /usr/bin/
+wget https://github.com/Kitware/CMake/releases/download/v3.19.1/cmake-3.19.1-Linux-x86_64.sh
+chmod +x cmake-3.19.1-Linux-x86_64.sh 
+./cmake-3.19.1-Linux-x86_64.sh --skip-license --prefix=/usr
 yum -y localinstall https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/c/czmq-3.0.2-3.el7.x86_64.rpm
 cd cspot/
 git submodule update --init --recursive
