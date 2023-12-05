1c049b07a226033823f2f7da95522e72038e5a2b
root <root@ip-172-31-34-206.compute.internal>
2023-12-05 23:35:45 +0000

    just a better set of defines

--- src/include/debug.h
+++ src/include/debug.h
@@ -12,11 +12,11 @@ void cspot_print_fatal_if(bool val, const char* format, ...);
 
 void cspot_print_timing(const char* format, ...);
 
-//#define QUIET
+#define QUIET
 
 //#define TRACK
 //#define TIMING
-#define DEBUG
+//#define DEBUG
 
 #ifdef QUIET
 #undef TIMING
