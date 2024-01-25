8cfa081b0784414b09f2eb14f03a2ca13b49b9aa
Ubuntu <ubuntu@ip-172-31-36-39.compute.internal>
2024-01-25 15:41:24 -0800

    getting ready to test

---
+++ apps/senspot/device-platform-ping.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+
+#include "woofc.h"
+
+#define ARGS "W:"
+char *Usage = "device-platform-ping -W woof_name for ping test on device\n";
+
+char Wname[4096];
+
+
+int main(int argc, char **argv)
+{
+	int c;
+	int err;
+	char wname[4096];
+	int randval;
+	int gval;
+	unsigned long seq_no;
+
+	memset(wname,0,sizeof(wname));
+
+	while((c = getopt(argc,argv,ARGS)) != EOF) {
+		switch(c) {
+			case 'W':
+				strncpy(wname,optarg,sizeof(wname));
+				break;
+			default:
+				fprintf(stderr,
+				"unrecognized command %c\n",(char)c);
+				fprintf(stderr,"%s",Usage);
+				exit(1);
+		}
+	}
+
+	if(wname[0] == 0) {
+		fprintf(stderr,"must specify woof for target device\n");
+		fprintf(stderr,"%s",Usage);
+		fflush(stderr);
+		exit(1);
+	}
+
+	randval = rand();
+
+	seq_no = WooFPut(wname,NULL,(void *)&randval);
+
+	if(WooFInvalid(seq_no)) {
+		fprintf(stderr,"device-platform-ping failed put for %s and value %d\n",
+			wname,
+			randval);
+		fflush(stderr);
+		exit(1);
+	}
+
+	err = WooFGet(wname,(void *)&gval,seq_no);
+
+	if(err < 0) {
+		fprintf(stderr,"device-platform-ping failed get for %s at seq_no %lu\n",
+			wname,
+			seq_no);
+		fflush(stderr);
+		exit(1);
+	}
+
+	if(randval != gval) {
+		fprintf(stderr,"device-platform-ping sent %d but got %d for %s at seq_no %lu\n",
+			randval,
+			gval,
+			wname,
+			seq_no);
+		fflush(stderr);
+		exit(1);
+	}
+
+	exit(0);
+}
+
+	
+
+	
+	
