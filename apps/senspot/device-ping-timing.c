0ceb7fdaeb4009d08bc36238aed224be939cce24
Ubuntu <ubuntu@ip-172-31-46-243.compute.internal>
2024-08-14 11:44:06 -0700

    added rtt store for arduino

--- apps/senspot/device-ping-timing.c
+++ apps/senspot/device-ping-timing.c
@@ -8,8 +8,7 @@
 #include "woofc.h"
 
 #define ARGS "W:s:"
-char *Usage = "device-ping-init -W woof_name\n\
-\t-s (history size in number of elements)\n";
+char *Usage = "device-ping-timing -W woof_name\n";
 
 char Wname[4096];
 char NameSpace[4096];
@@ -23,9 +22,11 @@ int main(int argc, char **argv)
 {
 	int c;
 	int err;
+	double rtt;
 	unsigned int payload;
 	char wname[4096];
 	unsigned long history_size;
+	unsigned long seqno;
 
 	memset(wname,0,sizeof(wname));
 	history_size = 0;
@@ -34,9 +35,6 @@ int main(int argc, char **argv)
 		switch(c) {
 			case 'W':
 				strncpy(wname,optarg,sizeof(wname));
-				break;
-			case 's':
-				history_size = atol(optarg);
 				break;
 			default:
 				fprintf(stderr,
@@ -53,33 +51,24 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	if(history_size == 0) {
-		fprintf(stderr,"must specify history size for target object\n");
-		fprintf(stderr,"%s",Usage);
-		fflush(stderr);
-		exit(1);
-	}
-	
 	WooFInit();
 
-	err = WooFCreate(wname,sizeof payload, history_size);
-
-	if(err < 0) {
-		fprintf(stderr,"device-ping-init failed for %s with history size %lu\n",
-			wname,
-			history_size);
-		fflush(stderr);
+	seqno = WooFGetLatestSeqno(wname);
+	if(seqno == (unsigned long)-1){
+		fprintf(stderr,"could not get last seqno from %s\n",
+				wname);
 		exit(1);
 	}
 
-	err = WooFCreate("ping-timing",sizeof(double), history_size);
-
-	if(err < 0) {
-		fprintf(stderr,"device-ping-init failed for ling timing  with history size %lu\n",
-			history_size);
-		fflush(stderr);
-		exit(1);
+	while(1) {
+		err = WooFGet(wname,&rtt,seqno);
+		if(err < 0) {
+			break;
+		}
+		printf("rtt: %f\n",rtt);
+		seqno--;
 	}
+
 
 
 	exit(0);
