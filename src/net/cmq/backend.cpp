8319f6187969eafcf3a15c632e549a3dab28c286
Ubuntu <ubuntu@ip-172-31-36-39.compute.internal>
2025-03-31 16:10:12 -0700

    trying to fix double free

--- src/net/cmq/backend.cpp
+++ src/net/cmq/backend.cpp
@@ -1,4 +1,5 @@
 #include <mutex>
+#include <pthread.h>
 #include "backend.hpp"
 
 #include <debug.h>
@@ -8,12 +9,17 @@
 
 namespace cspot::cmq {
 
+pthread_mutex_t shutdown_lock;
+
 void backend_shutdown(int sig) {
+	pthread_mutex_lock(&shutdown_lock);
 	cmq_pkt_shutdown();
+	pthread_mutex_unlock(&shutdown_lock);
 	return;
 }
 
 void backend_register() {
+    pthread_mutex_init(&shutdown_lock,NULL);
     signal(SIGINT,backend_shutdown);
     signal(SIGTERM,backend_shutdown);
     atexit(cmq_pkt_shutdown); // for mqtt xport
