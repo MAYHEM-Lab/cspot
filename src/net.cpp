15205828fd8bfdf58cb477e82693b80939d293e2
Ubuntu <ubuntu@ip-172-31-36-39.compute.internal>
2025-04-09 14:59:38 -0700

    might have multiprotocol working

--- src/net.cpp
+++ src/net.cpp
@@ -2,11 +2,16 @@
 #include <thread>
 #include "net.h"
 #include <pthread.h>
+#include <string.h>
 
 #include <debug.h>
 #include <functional>
 #include <unordered_map>
 #include <woofc-access.h>
+
+extern "C" {
+	extern unsigned int CMQ_use_mqtt;
+}
 
 namespace cspot {
 namespace zmq {
@@ -20,12 +25,16 @@ std::unique_ptr<network_backend> active_backend;
 namespace {
 struct registerer {
     registerer() {
+	if(CMQ_use_mqtt == 1) {
+        	zmq::backend_register();
+		cmq::backend_register();
+	} else {
 #ifdef USE_CMQ
-	cmq::backend_register();
+		cmq::backend_register();
 #else
-        zmq::backend_register();
-	cmq::backend_register();
+        	zmq::backend_register();
 #endif
+	}
 
         atexit([]{
             active_backend.reset();
@@ -76,21 +85,54 @@ void set_active_backend(std::unique_ptr<network_backend> backend) {
 } // namespace cspot
 
 extern "C" {
+extern unsigned int CMQ_use_mqtt;
+const char *backend_from_woof(const char *woof_name)
+{
+	if((strstr(woof_name,"woof://") != NULL) ||
+           (strstr(woof_name,"zmq://") != NULL)){
+		return("zmq");
+	} else if(strstr(woof_name,"cmq://") != NULL) {
+		return("cmq");
+	} else if(strstr(woof_name,"mqtt://") != NULL) {
+		return("mqtt");
+	} else {
+		return(NULL);
+	}
+}
+void adjust_active_backend(const char *woof_name) {
+	const char *be = backend_from_woof(woof_name);
+	if(be != NULL) {
+		if(strcmp(be,"zmq") == 0) {
+			cspot::set_active_backend(cspot::get_backend_with_name("zmq"));
+		} else if(strcmp(be,"cmq") == 0) {
+			CMQ_use_mqtt = 0;
+			cspot::set_active_backend(cspot::get_backend_with_name("cmq"));
+		} else if(strcmp(be,"mqtt") == 0) {
+			CMQ_use_mqtt = 1;
+			cspot::set_active_backend(cspot::get_backend_with_name("cmq"));
+		}
+	}
+	return;
+}
 unsigned long WooFMsgPut(const char* woof_name, const char* hand_name, const void* element, unsigned long el_size) {
-    return cspot::get_active_backend()->remote_put(woof_name, hand_name, element, el_size);
+	adjust_active_backend(woof_name);
+	return cspot::get_active_backend()->remote_put(woof_name, hand_name, element, el_size);
 }
 
 int WooFMsgGet(const char* woof_name, void* element, unsigned long el_size, unsigned long seq_no) {
-    return cspot::get_active_backend()->remote_get(woof_name, element, el_size, seq_no);
+	adjust_active_backend(woof_name);
+	return cspot::get_active_backend()->remote_get(woof_name, element, el_size, seq_no);
 }
 
 unsigned long WooFMsgGetElSize(const char* woof_name) {
-    return cspot::get_active_backend()->remote_get_elem_size(woof_name);
+	adjust_active_backend(woof_name);
+	return cspot::get_active_backend()->remote_get_elem_size(woof_name);
 }
 
 unsigned long
 WooFMsgGetLatestSeqno(const char* woof_name, const char* cause_woof_name, unsigned long cause_woof_latest_seq_no) {
-    return cspot::get_active_backend()->remote_get_latest_seq_no(woof_name, cause_woof_name, cause_woof_latest_seq_no);
+	adjust_active_backend(woof_name);
+    	return cspot::get_active_backend()->remote_get_latest_seq_no(woof_name, cause_woof_name, cause_woof_latest_seq_no);
 }
 
 unsigned long WooFMsgGetTail(const char* woof_name, void* elements, unsigned long el_size, int el_count) {
