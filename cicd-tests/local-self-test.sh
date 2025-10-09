a81223a0640784574b6b29d68dcb16003eaec106
Destructo <rich@cs.ucsb.edu>
2025-10-09 13:11:04 -0700

    still working on cleanup

--- cicd-tests/local-self-test.sh
+++ cicd-tests/local-self-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cp ../../apps/self-test/latency.sh .
 cp ../../apps/self-test/throughput.sh .
-`pwd`/woofc-namespace-platform -b spawn >& namespace.log &
-PPID=`ps auxww | grep woofc-namespace-platform | action_runner | grep -v grep | awk '{print $2}'`
+PWD=`pwd`
+$PWD/woofc-namespace-platform -b spawn >& namespace.log &
+PPID=`ps auxww | grep woofc-namespace-platform | grep $PWD | grep -v grep | awk '{print $2}'`
 LTEST=`./latency.sh 5 | grep "avg latency" | wc -l | awk '{print $1}'`
 if ( test $LTEST -eq 5 ) ; then
 	echo "SELF TEST 1 PASSED"
