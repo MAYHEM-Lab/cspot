8067747ef55306333f456b518818fff3deaefa3d
Destructo <rich@cs.ucsb.edu>
2025-10-14 10:28:20 -0700

    trying local network

---
+++ apps/self-test/throughput-net.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# run this in the same directory where woofc-namespace-platform is running
+HERE=`pwd`
+
+ADDR=$1
+COUNT=$2
+
+if ( test -z "$ADDR" ) ; then
+	echo "throughput-net.sh addr count"
+	exit 1
+fi
+
+if ( test -z "$COUNT" ) ; then
+        echo "throughput-net.sh count"
+        exit 1
+fi
+
+./stress-init -W zzzstress -s $(($COUNT*2))
+
+# do these in batches of 4 to stay under throttle limit
+
+./stress-test -V -W woof://$ADDR/zzzstress -s $COUNT -g 1 -p 1
+
---
+++ cicd-tests/local-network-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -f
+cp ../../apps/self-test/latency-net.sh .
+cp ../../apps/self-test/throughput-net.sh .
+#echo "$(pwd)"
+$(pwd)/woofc-namespace-platform -b spawn >& namespace.log &
+WPID=`ps auxww | grep "$(pwd)/woofc-namespace-platform" | grep -v grep | awk '{print $2}'`
+#echo $WPID
+LTEST=`./latency-net.sh 169.231.231.106 5 | grep "avg latency" | wc -l | awk '{print $1}'`
+if ( test $LTEST -eq 5 ) ; then
+	echo "SELF TEST 1 PASSED"
+else
+	echo "SELF TEST 1 FAILED"
+	rm -f zzzstress
+	kill -HUP $WPID
+	exit 1
+fi
+LTEST=`./throughput-net.sh 169.231.231.106 100 | grep "seq_no" | wc -l | awk '{print $1}'`
+if ( test $LTEST -eq 100 ) ; then
+	echo "SELF TEST 2 PASSED"
+else
+	echo "SELF TEST 2 FAILED"
+	rm -f zzzstress
+	kill -HUP $WPID
+	exit 1
+fi
+rm -f zzzstress
+#echo "sending HUP to $WPID"
+kill -HUP $WPID
+exit 0
+
+
