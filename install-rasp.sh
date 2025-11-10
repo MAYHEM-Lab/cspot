3ea33dc2179ae893696306106163245075659c3a
Destructo <rich@cs.ucsb.edu>
2025-11-10 14:32:53 -0800

    working on pi runner

--- install-rasp.sh
+++ install-rasp.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 
-apt-get update && apt-get -y install ninja-build cmake g++
-apt-get remove -y docker docker-engine docker.io containerd runc
-apt -y  install docker.io
+apt-get update && apt-get -y install cmake g++ libyaml-dev 
+#apt-get remove -y docker docker-engine docker.io containerd runc
+#apt -y  install docker.io
 git submodule update --init --recursive
 mkdir -p build
 cd build/
-cmake -G Ninja ..
-ninja
-ninja install
-docker pull racelab/cspot-docker-centos7:armv6
-docker tag racelab/cspot-docker-centos7:armv6 cspot-docker-centos7
+cmake ..
+make
+make install
+#docker pull racelab/cspot-docker-centos7:armv6
+#docker tag racelab/cspot-docker-centos7:armv6 cspot-docker-centos7
 cp ../SELF-TEST.sh ./bin
 
-if ! [[ $LD_LIBRARY_PATH == *"/usr/local/lib"* ]]; then
-    echo -e "if ! [[ \$LD_LIBRARY_PATH == *\"/usr/local/lib\"* ]]; then\nexport  LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:/usr/local/lib\"\nfi" >> ~/.bashrc
-    source ~/.bashrc
-fi
+#if ! [[ $LD_LIBRARY_PATH == *"/usr/local/lib"* ]]; then
+#    echo -e "if ! [[ \$LD_LIBRARY_PATH == *\"/usr/local/lib\"* ]]; then\nexport  LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:/usr/local/lib\"\nfi" >> ~/.bashrc
+#    source ~/.bashrc
+#fi
 cd ./bin
 ./SELF-TEST.sh
