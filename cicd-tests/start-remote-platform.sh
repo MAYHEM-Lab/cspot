99e56e2703d69ff3608c9e319a144588cafecb97
Destructo <rich@cs.ucsb.edu>
2025-11-14 14:20:56 -0800

    kill off the old one first

--- cicd-tests/start-remote-platform.sh
+++ cicd-tests/start-remote-platform.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
+#
+
+ssh -n ubuntu@169.231.231.109 "kill -9 `ps uaxww | grep woofc | grep actions_runner | awk '{print $2}'`"
 
 ssh -n ubuntu@169.231.231.109 'bash -c "cd /home/ubuntu/cspot/build/bin && nohup sh -c '\''setsid ./woofc-namespace-platform -b spawn > namespace.log 2>&1 < /dev/null &'\'' > /dev/null 2>&1 & disown"'
 
-ssh -n ubuntu@169.231.231.109 "ps auxww | grep woofc"
 
