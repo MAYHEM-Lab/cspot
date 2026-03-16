bf7f9f848a0bd9016c94bdd4e688887ff9dbb417
Ubuntu <liubov_kurafeeva@ucsb.edu>
2026-03-16 14:56:07 -0700

    working on cmq per tester

--- src/net/cmq/cmq-perf.c
+++ src/net/cmq/cmq-perf.c
@@ -30,18 +30,23 @@ int main(int argc, char **argv)
 	char host_ip[50];
 	unsigned long host_port;
 	int endpoint;
+	int server_sd;
 	int count;
-	char payload[1024];
+	unsigned char *payload;
 	int i;
 	unsigned char *fl;
 	unsigned char *f;
 	int err;
 	int size;
 	int is_server = 0;
+	struct timeval start_tv;
+	struct timeval end_tv;
+	unsigned char ack[1];
 
 	host_port = 8079;
 	memset(host_ip,0,sizeof(host_ip));
 	count = 1;
+	size = 8192;
 
 
 	while((c = getopt(argc,argv,ARGS)) != EOF) {
@@ -68,8 +73,8 @@ int main(int argc, char **argv)
 				exit(1);
 		}
 	}
-	if(host_ip[0] == 0) {
-		fprintf(stderr,"must specify server ip address\n");
+	if((is_server == 0) && (host_ip[0] == 0)) {
+		fprintf(stderr,"must specify server ip address in client mode\n");
 		fprintf(stderr,"%s",Usage);
 		exit(1);
 	}
@@ -79,293 +84,155 @@ int main(int argc, char **argv)
 		fprintf(stderr,"%s",Usage);
 		exit(1);
 	}
+
+	if(is_server == 1) {
+		printf("cmq-perf server listening on port %d\n",host_port);
+	} else {
+		printf("cmq-perf client connecting to server on host %s at port %d\n",
+			       host_ip,host_port);
+	}	
+	printf("\tsending %d messages of size %d\n",count,size);
+
 		
-	endpoint = cmq_pkt_connect(host_ip,host_port,TIMEOUT);
-	if(endpoint < 0) {
-		fprintf(stderr,"ERROR: failed to create endpoint\n");
-		exit(1);
-	}
-
-	err = cmq_frame_list_create(&fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to create frame list\n");
-		exit(1);
-	}
-
-	// create a frame list
-	for(i=0; i < count; i++) {
-		memset(payload,0,sizeof(payload));
-		sprintf(payload,"frame-%d",i);
-		printf("adding %s to frame list\n",(char *)payload);
-		err = cmq_frame_create(&f,(unsigned char *)payload,strlen(payload));
-		if(err < 0) {
-			fprintf(stderr,"ERROR: failed to create frame %d\n",i);
+	if(is_server == 0) {
+		payload = malloc(size);
+		if(payload == NULL) {
 			exit(1);
 		}
-		err = cmq_frame_append(fl,f);
-		if(err < 0) {
-			fprintf(stderr,"ERROR: failed to append frame %d\n",i);
+		endpoint = cmq_pkt_connect(host_ip,host_port,TIMEOUT);
+		if(endpoint < 0) {
+			fprintf(stderr,"ERROR: failed to create endpoint\n");
 			exit(1);
 		}
-	}
 
-	// send frame list to server
-	printf("sending frame list to server %s:%lu\n",host_ip,host_port);
-	err = cmq_pkt_send_msg(endpoint,fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to send msg\n");
-		exit(1);
-	}
-
-	// destroy the frame list
-	cmq_frame_list_destroy(fl);
-
-	// receive an echo of the frame list
-	printf("receiving frame list from server %s:%lu\n",host_ip,host_port);
-	err = cmq_pkt_recv_msg(endpoint,&fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to recv msg\n");
-		exit(1);
-	}
-
-	// print out the frame list echoed from the server
-	printf("printing echoed frame list\n");
-	while(!cmq_frame_list_empty(fl)) {
-		err = cmq_frame_pop(fl,&f);
+		err = cmq_frame_list_create(&fl);
 		if(err < 0) {
-			fprintf(stderr,"ERROR: could not pop frame\n");
+			fprintf(stderr,"ERROR: failed to create frame list\n");
 			exit(1);
 		}
-		memset(payload,0,sizeof(payload));
-		memcpy(payload,cmq_frame_payload(f),cmq_frame_size(f));
-		printf("echo: %s\n",(char *)payload);
-		cmq_frame_destroy(f);
-	}
 
-	cmq_frame_list_destroy(fl);
-
-	err = cmq_frame_list_create(&fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed create frame list for zero send\n");
-		exit(1);
-	}
-
-	printf("client send zero frame\n");
-	// now test zero frame send
-	err = cmq_frame_create(&f,NULL,0);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to create zero frame\n");
-		exit(1);
-	}
-
-	err = cmq_frame_append(fl,f);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to append zero frame for send\n");
-		exit(1);
-	}
-
-	err = cmq_pkt_send_msg(endpoint,fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to send zero frame\n");
-		exit(1);
-	}
-
-	cmq_frame_list_destroy(fl);
-	// recv a zero frame back
-	err = cmq_pkt_recv_msg(endpoint,&fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to recv zero frame\n");
-		exit(1);
-	}
-
-	err = cmq_frame_pop(fl,&f);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to pop zero frame\n");
-		exit(1);
-	}
-
-	cmq_frame_list_destroy(fl);
-
-	if(cmq_frame_size(f) != 0) {
-		fprintf(stderr,"ERROR: zero frame has size %d\n",
-				cmq_frame_size(f));
-		exit(1);
-	}
-	if(cmq_frame_payload(f) != NULL) {
-		fprintf(stderr,"ERROR: zero frame has non NULL payload\n");
-		exit(1);
-	}
-
-	cmq_frame_destroy(f);
-	printf("client recv zero frame echo\n");
-
-	cmq_pkt_close(endpoint);
-	return(0);
-}
-
-#endif
-
-#ifdef TESTSERVER
-
-#define PORT 8079
-#define TIMEOUT 3000 // 3 second accept timeout
-
-#define ARGS "p:"
-char *Usage = "cmq-test-server -p host_port\n";
-
-int main(int argc, char **argv)
-{
-	int c;
-	unsigned long host_port;
-	int endpoint;
-	int server_sd;
-	int count;
-	char payload[1024];
-	int i;
-	unsigned char *fl;
-	unsigned char *f;
-	int err;
-
-	host_port = 8079;
-
-	while((c = getopt(argc,argv,ARGS)) != EOF) {
-		switch(c) {
-			case 'p':
-				host_port = atoi(optarg);
-				break;
-			default:
-				fprintf(stderr,"unrecognized argument %c\n",
-					(char)c);
-				fprintf(stderr,"%s",Usage);
+		// create a frame list
+		for(i=0; i < count; i++) {
+			memset(payload,0,sizeof(payload));
+			sprintf(payload,"frame-%d",i);
+			printf("adding %s to frame list\n",(char *)payload);
+			err = cmq_frame_create(&f,(unsigned char *)payload,strlen(payload));
+			if(err < 0) {
+				fprintf(stderr,"ERROR: failed to create frame %d\n",i);
 				exit(1);
+			}
+			err = cmq_frame_append(fl,f);
+			if(err < 0) {
+				fprintf(stderr,"ERROR: failed to append frame %d\n",i);
+				exit(1);
+			}
 		}
-	}
-		
-	server_sd = cmq_pkt_listen(host_port);
-	if(server_sd < 0) {
-		fprintf(stderr,"ERROR: failed to create server_sd\n");
-		perror("listen");
-		exit(1);
-	}
 
-	endpoint = cmq_pkt_accept(server_sd, 0); // zero timeout implies wait forever
-	if(endpoint < 0) {
-		fprintf(stderr,"ERROR: failed to accept endpoint\n");
-		perror("listen");
-		exit(1);
-	}
-
-
-	err = cmq_pkt_recv_msg(endpoint,&fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to recv msg\n");
-		exit(1);
-	}
-
-	
-	// print out frame list without destroying it
-	printf("receiving frame list from client\n");
-	f = cmq_frame_list_head(fl);
-	if(f == NULL) {
-		fprintf(stderr,"ERROR: frame list head is NULL\n");
-		exit(1);
-	}
-	for(i=0; i < cmq_frame_list_count(fl); i++) {
-		memset(payload,0,sizeof(payload));
-		memcpy(payload,cmq_frame_payload(f),cmq_frame_size(f));
-		printf("recv: %s\n",(char *)payload);
-		f = cmq_frame_next(f);
-		if(f == NULL) {
-			break;
+		// send frame list to server
+		printf("sending frame list to server %s:%lu\n",host_ip,host_port);
+		err = cmq_pkt_send_msg(endpoint,fl);
+		if(err < 0) {
+			fprintf(stderr,"ERROR: failed to send msg\n");
+			exit(1);
 		}
-	}
 
-	if((f == NULL) && (i < (cmq_frame_list_count(fl)-1))) {
-		fprintf(stderr,"ERROR: NULL frame at frame %d\n",i);
-		exit(1);
-	}
+		// destroy the frame list
+		cmq_frame_list_destroy(fl);
+
+		// receive an ACK
+		printf("receiving frame list from server %s:%lu\n",host_ip,host_port);
+		err = cmq_pkt_recv_msg(endpoint,&fl);
+		if(err < 0) {
+			fprintf(stderr,"ERROR: failed to recv msg\n");
+			exit(1);
+		}
+		err = frame_list_pop(fl,&f);
+		if(err < 0) {
+			fprintf(stderr,"ERROR: failed to recv ack frame\n");
+			exit(1);
+		}
+		cmq_frame_destroy(f);
+		cmq_frame_list_destroy(fl);
+		exit(0);
+	} else { // i am the server
+		server_sd = cmq_pkt_listen(host_port);
+		if(server_sd < 0) {
+			fprintf(stderr,"ERROR: failed to create server_sd\n");
+			perror("listen");
+			exit(1);
+		}
+
+		while(1) {
+			endpoint = cmq_pkt_accept(server_sd, 0); // zero timeout implies wait forever
+			if(endpoint < 0) {
+				fprintf(stderr,"ERROR: failed to accept endpoint\n");
+				perror("listen");
+				exit(1);
+			}
+			err = cmq_pkt_recv_msg(endpoint,&fl);
+			if(err < 0) {
+				fprintf(stderr,"ERROR: failed to recv msg\n");
+				break;
+			}
+
 			
+			// print out frame list without destroying it
+			printf("receiving frame list from client\n");
+			f = cmq_frame_list_head(fl);
+			if(f == NULL) {
+				fprintf(stderr,"ERROR: frame list head is NULL\n");
+				break;
+			}
+			for(i=0; i < cmq_frame_list_count(fl); i++) {
+				printf("recv: %s\n",(char *)cmq_frame_payload(f));
+				f = cmq_frame_next(f);
+				if(f == NULL) {
+					break;
+				}
+			}
 
-	// send frame list to server
-	printf("sending frame list to client\n");
-	err = cmq_pkt_send_msg(endpoint,fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to send msg\n");
-		exit(1);
+			if((f == NULL) && (i < (cmq_frame_list_count(fl)-1))) {
+				fprintf(stderr,"ERROR: NULL frame at frame %d\n",i);
+				break;
+			}
+
+			cmq_frame_list_destroy(fl);
+
+			// send an ack
+			err = cmq_frame_list_create(&fl);
+			if(err < 0) {
+				fprintf(stderr,"ERROR: no frame list for ack\n");
+				break;
+			}
+			err = cmq_frame_create(&f,&ack[0],sizeof(ack));
+			if(err < 0) {
+				fprintf(stderr,"ERROR: no frame for ack\n");
+				break;
+			}
+
+			printf("sending ack to client\n");
+			err = cmq_pkt_send_msg(endpoint,fl);
+			if(err < 0) {
+				fprintf(stderr,"ERROR: failed to send msg\n");
+				break;
+			}
+
+			// destroy the frame list
+			cmq_frame_list_destroy(fl);
+			cmq_pkt_close(endpoint);
+		}
+		exit(0);
 	}
-
-	// destroy the frame list
-	cmq_frame_list_destroy(fl);
-
-	// test zero frame recv and echo
-	err = cmq_pkt_recv_msg(endpoint,&fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to recv zero frame\n");
-		exit(1);
-	}
-
-	err = cmq_frame_pop(fl,&f);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to pop zero frame\n");
-		exit(1);
-	}
-	cmq_frame_list_destroy(fl);
-
-	if(cmq_frame_size(f) != 0) {
-		fprintf(stderr,"ERROR: zero frame has size %d\n",
-				cmq_frame_size(f));
-		exit(1);
-	}
-
-	if(cmq_frame_payload(f) != NULL) {
-		fprintf(stderr,"ERROR: zero frame has non NULL payload\n");
-		exit(1);
-	}
-	cmq_frame_list_destroy(f);
-
-	printf("server recv zero frame\n");
-	// create zero frame response
-	
-	err = cmq_frame_list_create(&fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to create zero frame response list\n");
-		exit(1);
-	}
-
-	err = cmq_frame_create(&f,NULL,0);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to create zero frame response\n");
-		exit(1);
-	}
-
-	err = cmq_frame_append(fl,f);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to append zero frame response\n");
-		exit(1);
-	}
-	err = cmq_pkt_send_msg(endpoint,fl);
-	if(err < 0) {
-		fprintf(stderr,"ERROR: failed to send zero frame response\n");
-		exit(1);
-	}
-
-	cmq_frame_list_destroy(fl);
-	printf("server sent zero frame echo\n");
-
-	cmq_pkt_close(endpoint);
-	return(0);
 }
-
-#endif
-	
+		
 
 
-	
-	
-	
+		
+		
+		
 
-	
-	
+		
+		
 
-	
+		
 
