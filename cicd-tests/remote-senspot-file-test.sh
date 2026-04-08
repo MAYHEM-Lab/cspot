ee0a75fa9598781490aee8fb0927cd8e3f9d53f7
Destructo <rich@cs.ucsb.edu>
2026-04-08 15:22:43 -0700

    multi write and versioning

--- cicd-tests/remote-senspot-file-test.sh
+++ cicd-tests/remote-senspot-file-test.sh
@@ -11,13 +11,14 @@ echo "zzzfile created"
 ssh ubuntu@$ADDR "cd /home/ubuntu/cspot/build/bin && ./senspot-file-init -W zzzfilebig -M 64 -s 10"
 echo "zzzfilebig created"
 # create test file
-rm -f ./zzzfff
+rm -f ./zzzfff ./zzzxxx
 cp ../../cicd-tests/zzzfff .
+cp ../../cicd-tests/zzzxxx .
 
 FTEST=`./senspot-file-send -f ./zzzfff -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfile -V | grep "megabytes"`
 if ( test -z "$FTEST" ) ; then
 	echo "senspot-file-send failed"
-	rm -f ./zzzfff
+	rm -f ./zzzfff ./zzzxxx
 	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
@@ -27,7 +28,7 @@ RTEST=`./senspot-file-recv -f ./zzzggg -W woof://$ADDR/home/ubuntu/cspot/build/b
 
 if ( test -z "$RTEST" ) ; then
 	echo "senspot-file-recv failed"
-	rm -f ./zzzfff ./zzzggg
+	rm -f ./zzzfff ./zzzggg ./zzzxxx
 	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
@@ -37,7 +38,7 @@ DTEST=`diff ./zzzfff ./zzzggg`
 
 if ( ! test -z "$DTEST" ) ; then
 	echo "file diff failed"
-	rm -f ./zzzfff ./zzzggg
+	rm -f ./zzzfff ./zzzggg ./zzzxxx
 	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
@@ -46,7 +47,7 @@ fi
 FTEST=`./senspot-file-send -f ./zzzfff -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfilebig -V | grep "megabytes"`
 if ( test -z "$FTEST" ) ; then
 	echo "senspot-file-send big failed"
-	rm -f ./zzzfff
+	rm -f ./zzzfff ./zzzggg ./zzzxxx
 	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
@@ -56,7 +57,7 @@ RTEST=`./senspot-file-recv -f ./zzzggg -W woof://$ADDR/home/ubuntu/cspot/build/b
 
 if ( test -z "$RTEST" ) ; then
 	echo "senspot-file-recv big failed"
-	rm -f ./zzzfff ./zzzggg
+	rm -f ./zzzfff ./zzzggg ./zzzxxx
 	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
@@ -66,14 +67,58 @@ DTEST=`diff ./zzzfff ./zzzggg`
 
 if ( ! test -z "$DTEST" ) ; then
 	echo "file diff big failed"
-	rm -f ./zzzfff ./zzzggg
+	rm -f ./zzzfff ./zzzggg ./zzzxxx
 	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
 fi
 
+# try multi-write and versioning
+./senspot-file-send -f ./zzzfff -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfile &
+./senspot-file-send -f ./zzzfff -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfile &
+
+sleep 1
+FTEST=`./senspot-file-send -f ./zzzxxx -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfilebig -V | grep "megabytes"`
+
+if ( test -z "$FTEST" ) ; then
+	echo "second file send failed"
+	rm -f ./zzzfff ./zzzggg ./zzzxxx
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
+	$(pwd)/kill-remote-platform.sh 
+	exit 1
+fi
+
+VER=`./senspot-file-recv -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfile -L | head -n 2 | tail -n 1 | awk '{print $2}'`
+MAJ=`echo $VER | awk -F ':' '{print $1}'`
+MIN=`echo $VER | awk -F ':' '{print $2}'`
+
+echo "fetching version $VER"
+#./senspot-file-recv -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfile -L
+RTEST=`./senspot-file-recv -f ./zzzyyy -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfile -v $MAJ -m $MIN -V | grep "megabytes"`
+if ( test -z "$RTEST" ) ; then
+	echo "coupld not fetch version $VER"
+	rm -f ./zzzfff ./zzzggg ./zzzxxx ./zzzyyy
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
+	$(pwd)/kill-remote-platform.sh 
+	exit 1
+fi
+
+
+DTEST1=`diff ./zzzfff ./zzzyyy`
+DTEST2=`diff ./zzzxxx ./zzzyyy`
+if ( ! test -z "$DTEST1" ) ; then
+	if ( ! test -z "$DTEST2" ) ; then
+		echo "diff fails for multi version"
+		rm -f ./zzzfff ./zzzggg ./zzzxxx ./zzzyyy
+		ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
+		$(pwd)/kill-remote-platform.sh 
+		exit 1
+	fi
+fi
+
 $(pwd)/kill-remote-platform.sh 
 ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
+rm -f ./zzzfff ./zzzggg ./zzzxxx ./zzzyyy
 #echo "sending HUP to $WPID"
 #
 #
