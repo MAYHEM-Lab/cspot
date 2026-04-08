0c63d965bbc29888e6161cc9ea9f0e44783ec7ea
Destructo <rich@cs.ucsb.edu>
2026-04-08 12:35:16 -0700

    going to try file cicd

--- cicd-tests/remote-senspot-file-test.sh
+++ cicd-tests/remote-senspot-file-test.sh
@@ -1,25 +1,24 @@
 #!/bin/bash -f
 #
 source ../../../../../cicd-config.sh
+#source ~/actions-runner/cicd-config.sh
 ADDR=$X86HELPER
 $(pwd)/start-remote-platform.sh
 #do regular file tests first
-ssh ubuntu@$ADDR "cd /home/ubuntu/cspot/build/bin && ./senspot-file-init -W zzzfile -M 8 -s 1024"
+ssh ubuntu@$ADDR "cd /home/ubuntu/cspot/build/bin && ./senspot-file-init -W zzzfile -M 4 -s 1024"
 echo "zzzfile created"
 
+ssh ubuntu@$ADDR "cd /home/ubuntu/cspot/build/bin && ./senspot-file-init -W zzzfilebig -M 64 -s 10"
+echo "zzzfilebig created"
 # create test file
 rm -f ./zzzfff
-CNT=1
-while ( test $CNT -le 32000 ) ; do
-	echo $RANDOM >> ./zzzfff
-	CNT=$(($CNT+1))
-done
+cp ../../cicd-tests/zzzfff .
 
 FTEST=`./senspot-file-send -f ./zzzfff -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfile -V | grep "megabytes"`
 if ( test -z "$FTEST" ) ; then
 	echo "senspot-file-send failed"
 	rm -f ./zzzfff
-	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile"
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
 fi
@@ -29,23 +28,52 @@ RTEST=`./senspot-file-recv -f ./zzzggg -W woof://$ADDR/home/ubuntu/cspot/build/b
 if ( test -z "$RTEST" ) ; then
 	echo "senspot-file-recv failed"
 	rm -f ./zzzfff ./zzzggg
-	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile"
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
 fi
 
 DTEST=`diff ./zzzfff ./zzzggg`
 
-if ( test -z "$DTEST" ) ; then
+if ( ! test -z "$DTEST" ) ; then
 	echo "file diff failed"
 	rm -f ./zzzfff ./zzzggg
-	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile"
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
+	$(pwd)/kill-remote-platform.sh 
+	exit 1
+fi
+
+FTEST=`./senspot-file-send -f ./zzzfff -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfilebig -V | grep "megabytes"`
+if ( test -z "$FTEST" ) ; then
+	echo "senspot-file-send big failed"
+	rm -f ./zzzfff
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
+	$(pwd)/kill-remote-platform.sh 
+	exit 1
+fi
+
+RTEST=`./senspot-file-recv -f ./zzzggg -W woof://$ADDR/home/ubuntu/cspot/build/bin/zzzfilebig -V | grep "megabytes"`
+
+if ( test -z "$RTEST" ) ; then
+	echo "senspot-file-recv big failed"
+	rm -f ./zzzfff ./zzzggg
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
+	$(pwd)/kill-remote-platform.sh 
+	exit 1
+fi
+
+DTEST=`diff ./zzzfff ./zzzggg`
+
+if ( ! test -z "$DTEST" ) ; then
+	echo "file diff big failed"
+	rm -f ./zzzfff ./zzzggg
+	ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 	$(pwd)/kill-remote-platform.sh 
 	exit 1
 fi
 
 $(pwd)/kill-remote-platform.sh 
-ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile"
+ssh ubuntu@$ADDR "rm -f /home/ubuntu/cspot/build/bin/zzzfile*"
 #echo "sending HUP to $WPID"
 #
 
