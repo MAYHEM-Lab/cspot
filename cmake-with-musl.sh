4c9d274dd696610d4172c4f506f800f2893eb42a
Destructo <rich@cs.ucsb.edu>
2026-06-16 11:52:13 -0700

    moved the comment line

--- cmake-with-musl.sh
+++ cmake-with-musl.sh
@@ -4,9 +4,9 @@
 mkdir -p ./build
 HERE=`pwd`
 
+#  -DCMAKE_BUILD_TYPE=Debug \
 cmake -S . -B build \
   -DCMAKE_TOOLCHAIN_FILE=$HERE/toolchain-musl.cmake \
-#  -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_BUILD_TYPE=Release
 
 
