de9071eeb971160ef443e1ab6450a696baa24b98
Destructo <rich@cs.ucsb.edu>
2026-08-31 11:33:40 -0700

    updated for release

--- dist/update-cspot-distribution.sh
+++ dist/update-cspot-distribution.sh
@@ -116,12 +116,12 @@ fi
 
 # get the base recv binary if necessary
 if ( ! test -e "$HERE/senspot-file-recv.$TYPE" ) ; then
-	curl -fsSL https://raw.githubusercontent.com/MAYHEM-Lab/cspot/caplets/dist/senspot-file-recv.$TYPE -o $HERE/senspot-file-recv.$TYPE
+	curl -fsSL https://raw.githubusercontent.com/MAYHEM-Lab/cspot/dist/senspot-file-recv.$TYPE -o $HERE/senspot-file-recv.$TYPE
 	chmod 700 $HERE/senspot-file-recv.$TYPE
 fi
 
 if ( ! test -e "$HERE/update-cspot-distribution.sh" ) ; then
-	curl -fsSL https://raw.githubusercontent.com/MAYHEM-Lab/cspot/caplets/dist/update-cspot-distribution.sh -o $HERE/update-cspot-distribution.sh
+	curl -fsSL https://raw.githubusercontent.com/MAYHEM-Lab/cspot/dist/update-cspot-distribution.sh -o $HERE/update-cspot-distribution.sh
 	chmod 700 $HERE/update-cspot-distribution.sh
 fi
 
